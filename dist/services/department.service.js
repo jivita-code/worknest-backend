e62503149a8c9a03ef9faf600ecfdf04bfb3e2fe
Malinda Rasingolla <rasingollam@gmail.com>
2025-11-12 23:20:18 +0530

    fix: correct import path for PrismaClient in department service

--- dist/services/department.service.js
+++ dist/services/department.service.js
@@ -1,5 +1,5 @@
 // Department service
-import { PrismaClient } from "../../prisma/generated/prisma";
+import { PrismaClient } from "..//generated/prisma";
 const prisma = new PrismaClient();
 export const getDepartmentsDropdown = async (org_id) => {
     const departments = await prisma.department.findMany({
